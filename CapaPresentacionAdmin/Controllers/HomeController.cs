6991bde5571177367f51a06d811dd1a4e108cc74
Francisco Lozada <erjinsei.2210@gmail.com>
2023-06-11 21:20:24 -0500

    Agregar proyecto existe. Basado en un tutorial

---
+++ CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CapaEntidad;
+using CapaNegocio;
+
+namespace CapaPresentacionAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Usuarios()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListarUsuarios()
+        {
+            List<Usuario> oLista = new List<Usuario>();
+            oLista = new CN_Usuarios().Listar();
+
+
+            //que significa el segundo parametro de abajo?
+            return Json(new { data = oLista },JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GuardarUsuario(Usuario obj)
+        {
+
+            object resultado;
+            string mensaje = string.Empty;
+            if (obj.IdUsuario == 0)
+            {
+                resultado = new CN_Usuarios().Registrar(obj, out mensaje);
+            }
+            else
+            {
+                resultado = new CN_Usuarios().Editar(obj, out mensaje);
+            }
+            return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]  
+        public JsonResult EliminarUsuario(int id)
+        {
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+            respuesta = new CN_Usuarios().Eliminar(id, out mensaje);
+
+            return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
+
