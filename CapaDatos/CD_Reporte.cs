a1ab7cc4555f359e9195a3619eb8b55a9cf1e1fd
Francisco Lozada <erjinsei.2210@gmail.com>
2023-06-19 18:27:34 -0500

    Progreso hasta la Parte 25. Preparando la informacion de tabla de Ventas.

--- CapaDatos/CD_Reporte.cs
+++ CapaDatos/CD_Reporte.cs
@@ -6,11 +6,64 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace CapaDatos
 {
     public class CD_Reporte
     {
+
+        public List<Reporte> Ventas(string fechainicio, string fechafin, string idtransaccion)
+        {
+            List<Reporte> list = new List<Reporte>();
+            try
+            {
+
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+
+                    SqlCommand cmd = new SqlCommand("sp_ReporteVentas", oconexion); 
+                    cmd.Parameters.AddWithValue("FechaInicio", fechainicio);
+                    cmd.Parameters.AddWithValue("FechaFin", fechafin);
+                    cmd.Parameters.AddWithValue("IdTransaccion", idtransaccion);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+
+                        while (dr.Read())
+                        {
+                            list.Add(new Reporte()
+                            {
+
+
+                                FechaVenta = dr["FechaVenta"].ToString(),
+                                Cliente = dr["Cliente"].ToString(),
+                                Producto = dr["Producto"].ToString(),
+                                Precio = Convert.ToDecimal(dr["Precio"], new CultureInfo("es-CO")),
+                                Cantidad = Convert.ToInt32(dr["Cantidad"]),
+                                Total = Convert.ToDecimal(dr["Total"], new CultureInfo("es-CO")),
+                                IdTransaccion = dr["IdTransaccion"].ToString()
+
+                            });
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                list = new List<Reporte>();
+            }
+            return list;
+        }
+
+
 
         public Dashboard VerDashboard()
         {
@@ -51,5 +104,7 @@ namespace CapaDatos
 
             return objeto;
         }
+
+
     }
 }
